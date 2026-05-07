d431f6ff929bab3c0a8d89dc261b22a6067a0998
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-07 13:11:26 +0600

    Add insertion sort

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -22,7 +22,7 @@ int main(){
      }
     return 0;
 }
-int mergeSort(int arr, int left, int right){
+int mergeSort(int arr[], int left, int right){
     if(left<right){
         int mid= left+ (right-left)/2;
 
@@ -32,6 +32,41 @@ int mergeSort(int arr, int left, int right){
         merge(arr,left,mid,right);
     }
 }
-int merge(int arr, int left, int mid, int right){
+int merge(int arr[], int left, int mid, int right){
+    int n1= mid-left+1;
+    int n2= right-mid;
 
+    int L[n1], R[n2];
+
+    for(int i=0; i<n1; i++){
+        L[i]= arr[left+i];
+    }
+    for(int j=0; j<n2; j++){
+        R[j]= arr[mid+1+j];
+    }
+
+    int i=0, j=0, k=left;
+    while(i<n1 && j<n2){
+        if(L[i]<=R[j]){
+            arr[k]=L[i];
+            i++;
+        }
+        else{
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i<n1){
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+
+    while(j<n2){
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
 }
