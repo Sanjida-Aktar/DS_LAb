637b50057726eaeb4780eb5cbfa307bb43ba5bc0
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-06-20 23:58:44 +0600

    update balance braket

--- Stack/balance-braket.cpp
+++ Stack/balance-braket.cpp
@@ -10,17 +10,20 @@ int main(){
         if(s[i]=='('|| s[i]=='{' || s[i]=='['){
             st.push(s[i]);
         }
-        else if(s[i]==')' && !st.empty() && st.top()=='('){
-            st.pop();
-        }
-        else if(s[i]=='}' && !st.empty() && st.top()=='{'){
-            st.pop();
-        }
-        else if(s[i]==']' && !st.empty() && st.top()=='['){
-            st.pop();
-        }
-        else if(s[i]==')'|| s[i]=='}' || s[i]==']'){
-            st.push(s[i]);
+        else{
+            if(st.empty()){
+                cout<<"Not Balanced";
+                return 0;
+            }
+            if(s[i]==')' && st.top()=='(' ||
+             s[i]=='}' && st.top()=='{'||
+             s[i]==']' && st.top()=='['){
+                st.pop();
+             }
+             else{
+                cout<<"Not Balanced";
+                return 0;
+             }
         }
      }
      if(st.empty()){
